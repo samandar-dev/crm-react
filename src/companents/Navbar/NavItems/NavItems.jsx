e28f7302a40920eb18b48fa34b7e035a778c05a6
samandar-dev <samandarbohodirov01@gmail.com>
2022-06-20 08:07:41 +0500

    add

--- src/companents/Navbar/NavItems/NavItems.jsx
+++ src/companents/Navbar/NavItems/NavItems.jsx
@@ -6,8 +6,8 @@ function NavItems({ navBtns }) {
   return (
     <>
       {navBtns.map((itm, i) => (
-        <NavLink to={"/" + itm.pathName} key={itm.id} className="w-100">
-          <li className={`navbarr__nav__item ${itm.active ? "navitem-act" : ""}`} onClick={() => itm.active = true}>
+        <NavLink to={"/" + itm.pathName} key={itm.id} className={({ isActive }) => (isActive ? "navbarr__nav__item navitem-act" : "navbarr__nav__item w-100")}>
+          <li className={`navbarr__nav__item w-100 m-0 p-0  `} onClick={() => itm.active = true}>
             <button className='navbarr__nav__btn' id={itm.id}>
               <i className={itm.icon}></i> {itm.name}
             </button>
